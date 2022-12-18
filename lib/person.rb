2f73180f142b125bbd4f3eef550ed52f5063e07c
Maximilian Alba <albam476@gmail.com>
2022-12-18 18:32:15 -0500

    commit tree 09a3aff60d7356a876b3d033e08898381bb63b88

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,11 @@
+require 'pry'
+
 class Person
   # your code here
+  def initialize attribute_hash = nil
+    attribute_hash.each do |key, value|
+      self.class.attr_accessor(key) 
+      self.send("#{key}=", value)
+    end
+  end
 end
